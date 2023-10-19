7799c486c46024123c99e967a90010d5987d9a6e
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-10-20 00:50:19 +0530

    jkasd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+
+  end
+  def show
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: 'Pet Medical Report', # file name
+               template: 'layouts/pdf.html.erb',
+               layout: 'pdf.html', # optional, use 'pdf.html' for a simple layout
+               disposition: 'attachment' # 'attachment' to download, 'inline' to display in the browser
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+   get '/home/show', to: 'home#show'
   # Defines the root path route ("/")
    get 'home/index'
    root "home#index"
