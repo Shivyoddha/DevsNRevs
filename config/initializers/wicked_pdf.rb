dd4d3ec3ae0319a38825a4fdee17e500c1df586a
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-10-20 00:55:10 +0530

    zdfv

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,21 +1,30 @@
-# config/initializers/wicked_pdf.rb
-
-# Define a method to download and install wkhtmltopdf if it's missing
-def install_wkhtmltopdf_if_missing
-  unless system('which wkhtmltopdf')
-    # Download and install wkhtmltopdf
-    system('wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb')
-    system('sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb')
-  end
-end
-
-# Check for wkhtmltopdf on Rails startup
-install_wkhtmltopdf_if_missing
-
 # WickedPDF Global Configuration
+#
+# Use this to set up shared configuration options for your entire application.
+# Any of the configuration options shown here can also be applied to single
+# models by passing arguments to the `render :pdf` call.
+#
+# To learn more, check out the README:
+#
+# https://github.com/mileszs/wicked_pdf/blob/master/README.md
+
 WickedPdf.config = {
-  exe_path: '/usr/local/bin/wkhtmltopdf',
-  enable_local_file_access: true,
-  layout: 'pdf.html',
-  use_xvfb: true,
+  # Path to the wkhtmltopdf executable: This usually isn't needed if using
+  # one of the wkhtmltopdf-binary family of gems.
+  # exe_path: '/usr/local/bin/wkhtmltopdf',
+  #   or
+   exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
+
+  # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
+   enable_local_file_access: true,
+
+  # Layout file to be used for all PDFs
+  # (but can be overridden in `render :pdf` calls)
+  # layout: 'pdf.html',
+
+  # Using wkhtmltopdf without an X server can be achieved by enabling the
+  # 'use_xvfb' flag. This will wrap all wkhtmltopdf commands around the
+  # 'xvfb-run' command, in order to simulate an X server.
+  #
+  # use_xvfb: true,
 }
